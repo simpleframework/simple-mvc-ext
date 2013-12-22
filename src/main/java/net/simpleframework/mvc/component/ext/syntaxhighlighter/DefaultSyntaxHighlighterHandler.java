389f4d8ef3dee4fe31e3eb8a7e58740561043f2a
simpleframework <cknet@126.com>
2013-12-22 19:15:10 +0800

    Signed-off-by: simpleframework <cknet@126.com>

---
+++ src/main/java/net/simpleframework/mvc/component/ext/syntaxhighlighter/DefaultSyntaxHighlighterHandler.java
@@ -0,0 +1,13 @@
+package net.simpleframework.mvc.component.ext.syntaxhighlighter;
+
+import net.simpleframework.mvc.component.AbstractComponentHandler;
+
+/**
+ * Licensed under the Apache License, Version 2.0
+ * 
+ * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
+ *         http://www.simpleframework.net
+ */
+public class DefaultSyntaxHighlighterHandler extends AbstractComponentHandler implements
+		ISyntaxHighlighterHandler {
+}
---
+++ src/main/java/net/simpleframework/mvc/component/ext/syntaxhighlighter/ESyntaxHighlighterTheme.java
@@ -0,0 +1,25 @@
+package net.simpleframework.mvc.component.ext.syntaxhighlighter;
+
+/**
+ * Licensed under the Apache License, Version 2.0
+ * 
+ * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
+ *         http://www.simpleframework.net
+ */
+public enum ESyntaxHighlighterTheme {
+	shThemeDefault,
+
+	shThemeDjango,
+
+	shThemeEclipse,
+
+	shThemeEmacs,
+
+	shThemeFadeToGrey,
+
+	shThemeMDUltra,
+
+	shThemeMidnight,
+
+	shThemeRDark
+}
