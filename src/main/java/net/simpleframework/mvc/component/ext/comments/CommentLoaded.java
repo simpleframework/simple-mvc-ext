70eef86dba78f278b9cbcc1679744b2fb3bbdefb
CHENK <CHENK@CHENK-PC>
2016-08-25 16:39:48 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/mvc/component/ext/comments/CommentLoaded.java
+++ src/main/java/net/simpleframework/mvc/component/ext/comments/CommentLoaded.java
@@ -26,7 +26,8 @@ import net.simpleframework.mvc.component.ui.pager.PagerBean;
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
+ * @author 陈侃(cknet@126.com, 13910090885)
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public class CommentLoaded extends DefaultPageHandler {
@@ -67,7 +68,7 @@ public class CommentLoaded extends DefaultPageHandler {
 
 		// pager
 		pp.addComponentBean(commentName + "_pager", PagerBean.class)
-				.setPagerBarLayout(EPagerBarLayout.bottom)
+				.setPagerBarLayout(EPagerBarLayout.bottom).setShowEditPageItems(false)
 				.setContainerId("id" + commentName + "_pager").setHandlerClass(CommentList.class)
 				.setAttr("$comment", commentBean);
 	}
