fa74e677bf390d5ad5e4861d50d32195379eb847
CHENK <CHENK@CHENK-PC>
2016-09-09 20:12:22 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/mvc/component/ext/highchart/DataObj.java
+++ src/main/java/net/simpleframework/mvc/component/ext/highchart/DataObj.java
@@ -8,7 +8,8 @@ import net.simpleframework.mvc.component.ext.highchart.AbstractHcClass.AbstractH
  * 
  * www.highcharts.com/license
  * 
- * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
+ * @author 陈侃(cknet@126.com, 13910090885)
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public class DataObj extends AbstractHcElement<DataObj> implements IDataVal {
